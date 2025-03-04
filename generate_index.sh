79c741db6d2c5f12d19a78960f9026ec3a57977a
Dario Griffo <dariogriffo@gmail.com>
2025-03-04 21:55:14 +0000

    Change keys

--- generate_index.sh
+++ generate_index.sh
@@ -5,13 +5,13 @@ reprepro --dbdir $reprepro/db --confdir $reprepro/conf -C main includedeb bookwo
 reprepro --dbdir $reprepro/db --confdir $reprepro/conf -C main includedeb trixie $current/deb/trixie/*deb
 reprepro --dbdir $reprepro/db --confdir $reprepro/conf -C main includedeb sid $current/deb/sid/*deb
 cd dists/bookworm
-cat Release | gpg -s --default-key 3B9335DF576D3D58059C6AA50B56A1A69762E9FF -abs > Release.gpg
+cat Release | gpg -s --default-key EA0F721D231FDD3A0A17B9AC7808B4DD62C41256 -abs > Release.gpg
 cd -
 cd dists/trixie
-cat Release | gpg -s --default-key 3B9335DF576D3D58059C6AA50B56A1A69762E9FF -abs > Release.gpg
+cat Release | gpg -s --default-key EA0F721D231FDD3A0A17B9AC7808B4DD62C41256 -abs > Release.gpg
 cd -
 cd dists/sid
-cat Release | gpg -s --default-key 3B9335DF576D3D58059C6AA50B56A1A69762E9FF -abs > Release.gpg
+cat Release | gpg -s --default-key EA0F721D231FDD3A0A17B9AC7808B4DD62C41256 -abs > Release.gpg
 cd -
 cd ..
 
