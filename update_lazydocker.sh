f82328dd019a140381007a802872e90bf56a5ac0
Dario Griffo <dario@paytently.com>
2025-04-30 07:47:35 +0100

    Added scripts

---
+++ update_lazydocker.sh
@@ -0,0 +1,10 @@
+REPO=lazydocker-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=lazydocker
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./generate_index.sh
+git add .
+git commit -m "Update lazydocker to ${PACKAGE_VERSION}"
+git push -u origin main
+
---
+++ update_lowfi.sh
@@ -0,0 +1,10 @@
+REPO=lowfi-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=lowfi
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./generate_index.sh
+git add .
+git commit -m "Update lowfi to ${PACKAGE_VERSION}"
+git push -u origin main
+
