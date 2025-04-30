86fe2ab24548fd144cffcf3e5038ac3572ea97aa
Dario Griffo <dario@paytently.com>
2025-04-30 07:45:07 +0100

    Update uv to

---
+++ download_deb_file.sh
@@ -0,0 +1,13 @@
+REPO=$1
+PACKAGE_VERSION=$2
+BUILD_VERSION=$3
+PACKAGE_NAME=$4
+PWD=`pwd`
+declare -a arr=("bookworm" "trixie" "sid")
+for i in "${arr[@]}"
+do
+    DEBIAN_DIST=$i
+    cd deb/${DEBIAN_DIST}
+    wget https://github.com/dariogriffo/${REPO}/releases/download/${PACKAGE_VERSION}+${BUILD_VERSION}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${DEBIAN_DIST}_amd64.deb
+    cd ../..
+done
