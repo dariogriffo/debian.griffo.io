8efcdb9d8c1993bcc107d76d64fc4586f3064c2f
Dario Griffo <dario@paytently.com>
2025-05-17 11:00:02 +0100

    Update fzf to 0.62.0

---
+++ update_fzf.sh
@@ -0,0 +1,10 @@
+REPO=fzf-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=fzf
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./generate_index.sh
+git add .
+git commit -m "Update fzf to ${PACKAGE_VERSION}"
+git push -u origin main
+
