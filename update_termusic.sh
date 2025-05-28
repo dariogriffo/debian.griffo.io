acd59b14a6c7e55278b18d490fc30109e63ee7cd
Dario Griffo <dario@paytently.com>
2025-05-28 08:44:53 +0100

    Update termusic to

--- update_termusic.sh
+++ update_termusic.sh
@@ -2,7 +2,7 @@ REPO=termusic-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
 PACKAGE_NAME=termusic
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+#./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
 ./generate_index.sh
 git add .
 git commit -m "Update termusic to ${PACKAGE_VERSION}"
