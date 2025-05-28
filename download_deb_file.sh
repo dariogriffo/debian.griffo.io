20559bbee668f0a4a8c2abb20a2e2bbc209e17a7
Dario Griffo <dario@paytently.com>
2025-05-28 08:40:06 +0100

    Update termusic to 0.10.0

--- download_deb_file.sh
+++ download_deb_file.sh
@@ -8,6 +8,6 @@ for i in "${arr[@]}"
 do
     DEBIAN_DIST=$i
     cd deb/${DEBIAN_DIST}
-    wget https://github.com/dariogriffo/${REPO}/releases/download/${PACKAGE_VERSION}+${BUILD_VERSION}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${DEBIAN_DIST}_amd64.deb
+    wget https://github.com/dariogriffo/${REPO}/releases/download/${PACKAGE_VERSION}%2B${BUILD_VERSION}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${DEBIAN_DIST}_amd64.deb
     cd ../..
 done
