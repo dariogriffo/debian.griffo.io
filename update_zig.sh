bde8e2f6204bc106d0ce21c45dedbad8259dc022
Dario Griffo <dariogriffo@gmail.com>
2025-07-13 16:51:19 +0100

    Update zig to 0.14.1

---
+++ update_zig.sh
@@ -0,0 +1,10 @@
+REPO=zig-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=zig
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./generate_index.sh
+git add .
+git commit -m "Update zig to ${PACKAGE_VERSION}"
+git push -u origin main
+
