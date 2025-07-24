f95cbeeb8ce01444439141f8cf97ca37cc7ac1db
Dario Griffo <dariogriffo@gmail.com>
2025-07-24 20:24:41 +0100

    Update zls-master to 4

---
+++ update_zls_master.sh
@@ -0,0 +1,10 @@
+REPO=zls-master-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=zls-master
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./generate_index.sh
+git add .
+git commit -m "Update zls-master to ${BUILD_VERSION}"
+git push -u origin main
+
