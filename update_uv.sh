ea492c53453b9b5f41b7a8ddfbb3e4219e237e91
Dario Griffo <dariogriffo@gmail.com>
2025-08-16 13:28:18 +0100

    Update uv architectures

--- update_uv.sh
+++ update_uv.sh
@@ -2,6 +2,7 @@ REPO=uv-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
 PACKAGE_NAME=uv
+ARCHITECTURES="amd64,arm64,armel,armhf,ppc64el,s390x,riscv64"
 ./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
 ./generate_index.sh
 git add .
