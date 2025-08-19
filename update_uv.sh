42feb012d9b8037e19b16c76c5ae7335167ff91c
Dario Griffo <dariogriffo@gmail.com>
2025-08-19 08:28:06 +0100

    Update uv to 0.8.12+1

--- update_uv.sh
+++ update_uv.sh
@@ -3,9 +3,9 @@ PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
 PACKAGE_NAME=uv
 ARCHITECTURES="amd64,arm64,armel,armhf,ppc64el,s390x,riscv64"
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 ./generate_index.sh
 git add .
-git commit -m "Update uv to ${BUILD_VERSION}"
+git commit -m "Update uv to ${PACKAGE_VERSION}+${BUILD_VERSION}"
 git push -u origin main
 
