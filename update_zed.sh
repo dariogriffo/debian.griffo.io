c121f825438af367bf737dacdeb054c369c820d7
Dario Griffo <dariogriffo@gmail.com>
2025-10-20 14:16:59 +0100

    Add zed

---
+++ update_zed.sh
@@ -0,0 +1,10 @@
+REPO=zed-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=zed
+ARCHITECTURES="amd64,arm64"
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+./generate_index.sh
+
+
+
