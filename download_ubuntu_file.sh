0bf16148a69b0e524351821e4e67a18189cdb643
Dario Griffo <dariogriffo@gmail.com>
2026-02-19 15:33:16 +0000

    .

--- download_ubuntu_file.sh
+++ download_ubuntu_file.sh
@@ -9,8 +9,8 @@ declare -a architectures=(${ARCHITECTURES//,/ })
 for i in "${arr[@]}"
 do
     UBUNTU_DIST=$i
-    mkdir -p debian/deb/${UBUNTU_DIST}
-    cd debian/deb/${UBUNTU_DIST}
+    mkdir -p deb/${UBUNTU_DIST}
+    cd deb/${UBUNTU_DIST}
     for ARCH in ${architectures[@]}
     do
       wget https://github.com/dariogriffo/${REPO}/releases/download/${PACKAGE_VERSION}%2B${BUILD_VERSION}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${UBUNTU_DIST}_${ARCH}_ubu.deb || true
