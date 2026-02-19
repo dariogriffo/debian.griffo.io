1fa150233bbf7735a05812049d46dfc4ee48b11c
Dario Griffo <dariogriffo@gmail.com>
2026-02-19 15:34:20 +0000

    Update uv file

--- update_uv.sh
+++ update_uv.sh
@@ -2,8 +2,12 @@ REPO=uv-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
 PACKAGE_NAME=uv
+PWD=$(pwd)
+
 ARCHITECTURES="amd64,arm64,armel,armhf,ppc64el,s390x,riscv64,i386"
 ./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
 
 ARCHITECTURES="amd64,arm64,armhf,ppc64el,s390x,riscv64"
 ./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
