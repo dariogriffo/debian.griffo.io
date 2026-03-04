5fdb59febc7f853c5b0743b0558a6e8a17ff914e
Dario Griffo <dariogriffo@gmail.com>
2026-03-04 08:50:17 +0000

    Add deno deployment

---
+++ update_deno.sh
@@ -0,0 +1,17 @@
+REPO=deno-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+
+PACKAGE_NAME=deno
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+PACKAGE_NAME=denort
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+PACKAGE_NAME=deno
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+PACKAGE_NAME=denort
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+PACKAGE_NAME=deno
+./download_udenotu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+PACKAGE_NAME=denort
+./download_udenotu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
