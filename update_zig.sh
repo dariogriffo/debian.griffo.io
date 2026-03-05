d1adf890812fb46de33ad911ed7dfce1762e6490
Dario Griffo <dariogriffo@gmail.com>
2026-03-05 13:55:53 +0000

    Update zig

--- update_zig.sh
+++ update_zig.sh
@@ -1,9 +1,17 @@
 REPO=zig-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
-PACKAGE_NAME=zig
 
+PACKAGE_NAME=zig
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+PACKAGE_NAME=zig-zero
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+PACKAGE_NAME=zig
 ./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
 PACKAGE_NAME=zig-zero
 ./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
-./generate_index.sh
+PACKAGE_NAME=zig
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+PACKAGE_NAME=zig-zero
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
