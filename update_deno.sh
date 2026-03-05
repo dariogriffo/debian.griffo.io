5b183212cc5d296b28b303bb30ddd291771f2f9e
Dario Griffo <dariogriffo@gmail.com>
2026-03-05 20:50:49 +0000

    support more architectures for deno and zig

--- update_deno.sh
+++ update_deno.sh
@@ -1,6 +1,7 @@
 REPO=deno-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
+ARCHITECTURES="amd64,arm64"
 
 PACKAGE_NAME=deno
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
@@ -8,10 +9,10 @@ PACKAGE_NAME=denort
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
 
 PACKAGE_NAME=deno
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 PACKAGE_NAME=denort
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 PACKAGE_NAME=deno
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 PACKAGE_NAME=denort
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
--- update_zig.sh
+++ update_zig.sh
@@ -1,6 +1,7 @@
 REPO=zig-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
+ARCHITECTURES="amd64,arm64,armel,armhf,ppc64el,s390x,riscv64,i386"
 
 PACKAGE_NAME=zig
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
@@ -8,10 +9,10 @@ PACKAGE_NAME=zig-zero
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
 
 PACKAGE_NAME=zig
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 PACKAGE_NAME=zig-zero
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 PACKAGE_NAME=zig
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 PACKAGE_NAME=zig-zero
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
