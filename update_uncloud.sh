baf637eb37fa424638acdd2c85bad56ab795a2ea
Dario Griffo <dariogriffo@gmail.com>
2026-04-14 09:20:48 +0100

    Publish all

--- update_uncloud.sh
+++ update_uncloud.sh
@@ -1,8 +1,18 @@
 REPO=uncloud-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
+ARCHITECTURES="amd64,arm64"
 PACKAGE_NAME=uncloud
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+PACKAGE_NAME=uncloudd
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
 
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+PACKAGE_NAME=uncloud
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+PACKAGE_NAME=uncloudd
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+PACKAGE_NAME=uncloud
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+PACKAGE_NAME=uncloudd
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
