45d38fb126d888f74798609449845088ec0100ec
Dario Griffo <dariogriffo@gmail.com>
2026-05-27 15:45:22 +0100

    Update forgejo updater

--- update_forgejo.sh
+++ update_forgejo.sh
@@ -4,5 +4,8 @@ BUILD_VERSION=${2:-1}
 PACKAGE_NAME=forgejo
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
 
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+ARCHITECTURES="amd64,arm64,armhf"
+
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
