30988de51fe59c1ac77480255c82cb39255aff23
Dario Griffo <dariogriffo@gmail.com>
2026-06-15 11:26:03 +0100

    Add viu

---
+++ update_viu.sh
@@ -0,0 +1,13 @@
+REPO=viu-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=viu
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+
+cd ${PWD}
+
+find src/ -type f -name "*${PACKAGE_NAME}*" -delete 2>/dev/null || true
+./download_src_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
