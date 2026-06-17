a3211fe58375775aeb451c1e6cbe0abd5147e4a2
Dario Griffo <dariogriffo@gmail.com>
2026-06-17 14:55:57 +0100

    update some architectures

--- update_lazydocker.sh
+++ update_lazydocker.sh
@@ -2,10 +2,12 @@ REPO=lazydocker-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
 PACKAGE_NAME=lazydocker
+ARCHITECTURES="amd64,arm64,armel,armhf,i386"
+
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
 
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 
 cd ${PWD}
 
