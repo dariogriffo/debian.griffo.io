1ab3919c0a8dec3e49431a759409af12ebfb60eb
Dario Griffo <dariogriffo@gmail.com>
2026-07-05 10:01:25 +0100

    zoxide multi-arch

--- update_zoxide.sh
+++ update_zoxide.sh
@@ -2,10 +2,12 @@ REPO=zoxide-debian
 PACKAGE_VERSION=$1
 BUILD_VERSION=${2:-1}
 PACKAGE_NAME=zoxide
+ARCHITECTURES="amd64,arm64,armhf,riscv64"
+
 find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
 
-./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
-./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
 
 cd ${PWD}
 
