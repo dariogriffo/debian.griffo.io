453f57db7923f8050f2d02739abad698b4e04910
Dario Griffo <dariogriffo@gmail.com>
2026-07-10 10:10:16 +0100

    Support ~dist package names in download scripts

--- download_oldstable_deb_file.sh
+++ download_oldstable_deb_file.sh
@@ -14,7 +14,19 @@ do
     cd deb/${DEBIAN_DIST}
     for ARCH in ${architectures[@]}
     do
-      wget https://github.com/dariogriffo/${REPO}/releases/download/${RELEASE_VERSION}%2B${BUILD_VERSION}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${DEBIAN_DIST}_${ARCH}.deb || true
+      BASE_URL=https://github.com/dariogriffo/${REPO}/releases/download/${RELEASE_VERSION}%2B${BUILD_VERSION}
+      # Newer repos use ~dist in the package version; GitHub mangles ~ to . in
+      # release asset names, so try ~, then the mangled . name (saved back under
+      # the ~ name), then fall back to the legacy +dist name.
+      TILDE_NAME=${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}~${DEBIAN_DIST}_${ARCH}.deb
+      DOT_NAME=${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}.${DEBIAN_DIST}_${ARCH}.deb
+      if wget ${BASE_URL}/${TILDE_NAME}; then
+        :
+      elif wget ${BASE_URL}/${DOT_NAME}; then
+        mv ${DOT_NAME} ${TILDE_NAME}
+      else
+        wget ${BASE_URL}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${DEBIAN_DIST}_${ARCH}.deb || true
+      fi
     done
     cd ../..
 done
--- download_ubuntu_file.sh
+++ download_ubuntu_file.sh
@@ -13,7 +13,19 @@ do
     cd deb/${UBUNTU_DIST}
     for ARCH in ${architectures[@]}
     do
-      wget https://github.com/dariogriffo/${REPO}/releases/download/${PACKAGE_VERSION}%2B${BUILD_VERSION}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${UBUNTU_DIST}_${ARCH}_ubu.deb || true
+      BASE_URL=https://github.com/dariogriffo/${REPO}/releases/download/${PACKAGE_VERSION}%2B${BUILD_VERSION}
+      # Newer repos use ~dist in the package version; GitHub mangles ~ to . in
+      # release asset names, so try ~, then the mangled . name (saved back under
+      # the ~ name), then fall back to the legacy +dist name.
+      TILDE_NAME=${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}~${UBUNTU_DIST}_${ARCH}_ubu.deb
+      DOT_NAME=${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}.${UBUNTU_DIST}_${ARCH}_ubu.deb
+      if wget ${BASE_URL}/${TILDE_NAME}; then
+        :
+      elif wget ${BASE_URL}/${DOT_NAME}; then
+        mv ${DOT_NAME} ${TILDE_NAME}
+      else
+        wget ${BASE_URL}/${PACKAGE_NAME}_${PACKAGE_VERSION}-${BUILD_VERSION}+${UBUNTU_DIST}_${ARCH}_ubu.deb || true
+      fi
     done
     cd ../..
 done
