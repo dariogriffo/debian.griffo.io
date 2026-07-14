98dc14b16e1faf5bbd09c5e40416fae9bf0ace66
Dario Griffo <dariogriffo@gmail.com>
2026-07-14 21:26:31 +0100

    Add update scripts for ruff, fastfetch, yq

---
+++ update_ruff.sh
@@ -0,0 +1,20 @@
+REPO=ruff-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=ruff
+PWD=$(pwd)
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+ARCHITECTURES="amd64,arm64,armel,armhf,ppc64el,s390x,riscv64,i386"
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+ARCHITECTURES="amd64,arm64,armhf,ppc64el,s390x,riscv64"
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+find src/ -type f -name "*${PACKAGE_NAME}*" -delete 2>/dev/null || true
+./download_src_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
+
