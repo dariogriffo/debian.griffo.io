392324687aefff94e51f1f207b551dc19777feb0
Dario Griffo <dariogriffo@gmail.com>
2026-07-14 20:02:47 +0100

    Add update_oh_my_posh.sh (amd64/arm64/armhf, Debian + Ubuntu + source)

---
+++ update_oh_my_posh.sh
@@ -0,0 +1,19 @@
+REPO=oh-my-posh-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=oh-my-posh
+PWD=$(pwd)
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+ARCHITECTURES="amd64,arm64,armhf"
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+ARCHITECTURES="amd64,arm64,armhf"
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+find src/ -type f -name "*${PACKAGE_NAME}*" -delete 2>/dev/null || true
+./download_src_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
