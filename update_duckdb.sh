180314cfc80c597fc81923db761147e4c28f0a31
Dario Griffo <dariogriffo@gmail.com>
2026-07-19 07:23:00 +0100

    Add more packages

---
+++ update_duckdb.sh
@@ -0,0 +1,18 @@
+REPO=duckdb-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=duckdb
+PWD=$(pwd)
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+ARCHITECTURES="amd64,arm64"
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+find src/ -type f -name "*${PACKAGE_NAME}*" -delete 2>/dev/null || true
+./download_src_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
