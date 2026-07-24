02b52857a6e9b53a622a5be7aa0dbfa21107bd55
Dario Griffo <dariogriffo@gmail.com>
2026-07-24 13:07:49 +0100

    Add update_zapzap.sh to stage zapzap debs and source into the apt repo

---
+++ update_zapzap.sh
@@ -0,0 +1,19 @@
+REPO=zapzap-debian
+PACKAGE_VERSION=$1
+BUILD_VERSION=${2:-1}
+PACKAGE_NAME=zapzap
+PWD=$(pwd)
+find deb/ -mindepth 2 -type f -name "*${PACKAGE_NAME}*" -delete
+
+# ZapZap only ships amd64.
+ARCHITECTURES="amd64"
+./download_deb_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+./download_ubuntu_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME} ${ARCHITECTURES}
+
+cd ${PWD}
+
+find src/ -type f -name "*${PACKAGE_NAME}*" -delete 2>/dev/null || true
+./download_src_file.sh ${REPO} ${PACKAGE_VERSION} ${BUILD_VERSION} ${PACKAGE_NAME}
